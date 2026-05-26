246b27106a0a8fa6a831ec7e8ba025f80e3a8acd
Meerah <k.meerah@google.com>
2026-05-27 01:05:40 +0200

    Add CI Pipelines with Github Actions

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const express = require('express');
+
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
     res.send("Welcome to ShopEase from Meerah!");
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app;
---
+++ test/app.test.js
@@ -0,0 +1,13 @@
+const request = require('supertest');
+const app = require('../app');
+
+describe('GET /', () => {
+  it('should return 200 status and correct message', async () => {
+    const res = await request(app).get('/');
+
+    expect(res.status).toBe(200);
+    expect(res.text).toBe("Welcome to ShopEase from Meerah!");
+  });
+});
+
+
